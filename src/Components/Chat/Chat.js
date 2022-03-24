0053764967d2c0f089f285b9b74198b638f0f56f
itprogaur <professionalcodehub@gmail.com>
2022-03-24 23:39:44 +0530

    File builded

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -10,7 +10,7 @@ import TextContainer from "../TextContainer/TextContainer";
 let socket;
 
 function Chat({ location }) {
-  const ENDPOINT = "http://localhost:5000/";
+  const ENDPOINT = "https://react-realchat-application.herokuapp.com/ ";
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
